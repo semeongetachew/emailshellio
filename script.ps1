ba27081261cdcaff0ad01bdffd85e20e12a89a84
Semeon Getachew <getachewsemeon@gmail.com>
2020-06-09 10:40:23 -0400

    created script

---
+++ script.ps1
@@ -0,0 +1,22 @@
+$MyEmail = "emailaddress@gmail.com"
+$SMTP= "smtp.gmail.com"
+$To = "emailaddress@gmail.com"
+$Body = "This is the body"
+$Creds = (Get-Credential -Credential "$MyEmail")
+
+Start-Sleep 2
+
+Send-MailMessage -To $to -From $MyEmail -Subject $Subject -Body $Body -SmtpServer $SMTP -Credential $Creds -UseSsl -Port 587 -DeliveryNotificationOption never
+
+<# 
+$PSEmailServer variable can be used to pre-configure the
+SMTP server in your Powershell Profile. Then you don't need
+to specify -smtpserver paramter. Send-MailMessage will use the
+SMTP sever address assigned to $PSEmailServer
+Delivery Notification Options:
+-- None: No notification.        
+-- OnSuccess: Notify if the delivery is successful.      
+-- OnFailure: Notify if the delivery is unsuccessful.     
+-- Delay: Notify if the delivery is delayed.       
+-- Never: Never notify.
+#>
